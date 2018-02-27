ece5e49e676e7db1cee1d049b3afe6708a11bb7c
FallenXP <expgamer303@gmail.com>
2018-02-27 16:24:06 -0500

    Base Bot

---
+++ bot.js
@@ -0,0 +1,14 @@
+//Requires
+Discord = require("discord.js");
+color = require("colors");
+
+client = new Discord.Client();
+
+//If Command Is
+function commandIs(str, msg) {
+    return msg.content.toLowerCase().startsWith("f!" + str);
+}
+
+
+
+client.login("process.env.token");
