c52a7b65c1faf7bc49d0f9f1a1cb176fb8a0dbed
FallenXP <expgamer303@gmail.com>
2018-03-02 16:46:55 -0500

    fixed things

--- modules/say.mod.js
+++ modules/say.mod.js
@@ -6,10 +6,10 @@ exports.onLoad = api => {
         
         if(args.length === 1) {
             msg.channel.send("You did not define an argument. Usage: `f!say [message to say]`");
-            logActions(msg, `${sender} did not enter an argument for the say command`)
+            // logActions(msg, `${sender} did not enter an argument for the say command`);
         }else {
             msg.channel.send(say);
-            logActions(msg, `${sender} used the say command to say ${say}.`);
+            // logActions(msg, `${sender} used the say command to say ${say}.`);
         }  
     })
 };
