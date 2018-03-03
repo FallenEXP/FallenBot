331537b2fc903479406d01f2ac32e238b71fd371
FallenXP <expgamer303@gmail.com>
2018-03-02 21:34:06 -0500

    fixed ping

--- modules/ping.mod.js
+++ modules/ping.mod.js
@@ -1,13 +1,14 @@
 exports.id = 'ping';
 exports.onLoad = api => {
     api.commands.add('ping', msg => {
+        var ping = Math.round(api.client.ping);
         var pnog = Math.floor(Math.random() * 10) + 1;
-        
+
         if(pnog == 1) {
-            msg.channel.send(`PNOG! :ping_pong: ${api.client.pings[0]}ms`);
+            msg.channel.send(`PNOG! :ping_pong: ${ping}ms`);
             // logActions(msg, `PNOGED, ${sender}!`);
         }else {
-            msg.channel.send(`PONG! :ping_pong: ${api.client.pings[0]}ms`);
+            msg.channel.send(`PONG! :ping_pong: ${ping}ms`);
             // logActions(msg, `PONGED, ${sender}!`);
         }
     })
